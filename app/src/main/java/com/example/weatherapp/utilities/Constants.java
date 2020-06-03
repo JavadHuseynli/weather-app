b1042093536148d9083b708e31b5bab69e35382f
JavadHuseynli <cavadhuseynli98@gmail.com>
2020-06-03 16:08:04 +0400

    Wheather first app

---
+++ app/src/main/java/com/example/weatherapp/utilities/Constants.java
@@ -0,0 +1,15 @@
+package com.example.weatherapp.utilities;
+
+/**
+ * @author vishal kumar
+ * @version 1.0
+ * @since 06.01.2019
+ * <p>
+ * Constants class contains all the configurable variables
+ */
+public class Constants {
+    public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    public static final String API_KEY = "490a8404b8ea03e8e7c470d03835af18";
+    public static final String UNITS = "metric";
+    public static String TEMP_UNIT = "";
+}
